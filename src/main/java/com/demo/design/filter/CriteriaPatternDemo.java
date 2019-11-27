a2472bc695d0edc29b464f284a6a73676fbc5d76
zhaoxingwu <zhaoxingwu@AT10909-DLP.corp.autohome.inc>
2019-11-27 15:06:19 +0800

    新增 桥接模式；过滤者模式

---
+++ src/main/java/com/demo/design/filter/CriteriaPatternDemo.java
@@ -0,0 +1,33 @@
+package com.demo.design.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaPatternDemo {
+	public static void main(String[] args) {
+		List<Person> persons = new ArrayList<Person>();
+
+		persons.add(new Person("Robert", "Male", "Single"));
+		persons.add(new Person("John", "Male", "Married"));
+		persons.add(new Person("Laura", "Female", "Married"));
+		persons.add(new Person("Diana", "Female", "Single"));
+		persons.add(new Person("Mike", "Male", "Single"));
+		persons.add(new Person("Bobby", "Male", "Single"));
+
+		Criteria male = new CriteriaMale();
+		Criteria female = new CriteriaFemale();
+
+		System.out.println("Males: ");
+		printPersons(male.meetCriteria(persons));
+
+		System.out.println("\nFemales: ");
+		printPersons(female.meetCriteria(persons));
+	}
+	
+	public static void printPersons(List<Person> persons) {
+		for (Person person : persons) {
+			System.out.println("Person : [ Name : " + person.getName() + ", Gender : " + person.getGender()
+					+ ", Marital Status : " + person.getMaritalStatus() + " ]");
+		}
+	}
+}
